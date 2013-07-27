dd25960915670fab540d37e33797f9ef17c08f4e
Jon Cantwell <jon.c.cantwell@gmail.com>
2013-07-26 20:53:54 -0400

    added board constants and integer_state function

--- Board.py
+++ Board.py
@@ -1,5 +1,17 @@
 class Board:
   
+    CATSGAME_1 = 3888
+    CATSGAME_2 = 2592
+    
+    # All possible configurations that could contain a 3-in-a-row
+    TOP      = [0,1,2]
+    LEFT     = [0,3,6]
+    RIGHT    = [2,5,8]
+    BOTTOM   = [6,7,8]
+    MIDROW   = [3,4,5]
+    MIDCOL   = [1,4,7]
+    DIAG1    = [0,4,8]
+    DIAG2    = [2,4,6]
     # 'marks' maintains a list of integers corresponding to the present board      
     # state. It is initialised to a completely blank board by default.
     # '1' corresponds to a blank spot on the board.
@@ -54,7 +66,8 @@ class Board:
             return "?"
 
     def game_over(self):
-        return False
+        integer_state = self.integer_state()
+        if integer_state == 3888 or integer_state == 2592:
     
     # assumes that position is a legal move. The check for that -should- be in this function,
     # but I didn't want to spend forever on fiddly game loop UI things. I might refactor that later?
@@ -70,6 +83,15 @@ class Board:
     def is_legal_move(self, position):
         return self.marks[position] == 1
     
+    # returns a normalised integer state of the game board, determined by
+    # multiplying the contents of self.marks together.
+    # see also: http://en.wikipedia.org/wiki/G%C3%B6del_numbering ish.
+    # states of note:
+    # 3888 and 2592 are the only possible cat's games (5 X and 4 O, or 5 O and 4 X)
+    # (see constants at top of class)
+    def integer_state(self):
+        return reduce(lambda x, y: x*y, self.marks)
+
 #boardtest = Board()
 #boardtest2 = Board([1,2,3,1,1,1,1,3,2])
 
